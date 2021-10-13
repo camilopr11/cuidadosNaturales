f252ddbcd3b31291e27a62bdc6f1331a8f77f018
camilopr11 <camilop.rivas@gmail.com>
2021-10-12 21:23:06 -0500

    Restructuración rutas y controladores de enciclopedia

--- src/routes/enciclopedia.routes.js
+++ src/routes/enciclopedia.routes.js
@@ -1,33 +1,145 @@
-import { Router } from 'express'
-import { connect } from '../database/connection'
-import enciclopediaController from '../controllers/enciclopedia_controller'
-const router = Router()
+const express = require('express')
+const router = express.Router()
+const _controller = require('../controllers/enciclopedia_controller')
 
-//Ruta para listar
-router.get('/enciclopedia/listInfo', async (req, res) => {
-    const enciclopedia = await new enciclopediaController().listInfo()
-    res.json(enciclopedia)
+
+/**
+ * Petición: Traer toda la información de enciclopedias
+ * Parámetros: Vacío
+ * Cuerpo: Vacío
+ * Respuesta: Información consultada o mensaje de error
+ */
+router.get('/enciclopedias/', async (req, res) => {
+    
+    _controller
+    .listInfo()
+        .then((enciclopedia) => {
+            res.statusCode = 200
+            res.setHeader("Content_type", "application/json")
+            res.json(enciclopedia)
+        })
+        .catch(error => {
+            res.send(error);
+        })
 })
 
+/**
+ * Petición: Buscar información de enciclopedias mediante id
+ * Parámetros: id
+ * Cuerpo: Vacío
+ * Respuesta: Información consultada o mensaje de error
+ */
+router.get('/enciclopedias/:enciclopediaId', async (req, res) => {
+    let id = req.params.enciclopediaId
+    _controller
+    .findById(id)
+        .then((enciclopedia) => {
+            res.statusCode = 200
+            res.setHeader("Content_type", "application/json")
+            res.json(enciclopedia)
+        })
+        .catch(error => {
+            res.send(error)
+        })
+})
 
-//Rutas para agregar 
+/**
+ * Petición: Buscar información de enciclopedias mediante título
+ * Parámetros: title
+ * Cuerpo: Vacío
+ * Respuesta: Información consultada o mensaje de error
+ */
+router.get('/enciclopedias/title/:title', async (req, res) => {
+    let title = req.params.title
+    _controller
+    .findByTitle(title)
+        .then((enciclopedia) => {
+            res.statusCode = 200
+            res.setHeader("Content_type", "application/json")
+            res.json(enciclopedia)
+        })
+        .catch(error => {
+            res.send(error)
+        })
+})
 
-router.post('/enciclopedia/new-info', async (req, res) => {
+/**
+ * Petición: Agregar nueva información a enciclopedias
+ * Parámetros: Vacío
+ * Cuerpo: title, description, content, category, date
+ * Respuesta: mensaje de éxito o error
+ */
+router.post('/enciclopedias/', async (req, res) => {
     const info = {
         title: req.body.title, 
         description: req.body.description, 
         content: req.body.content, 
-        category: req.body.category 
+        category: req.body.category,
+        date: req.body.date
     } 
-    const result = await new enciclopediaController.createInfo(info)
+    _controller
+    .saveInfo(info)
+        .then((response) => {
+            res.setHeader("Content-Type", "application/json")
+            res.status(200).json({
+                success: true,
+                "data": response
+            })
+        })
+        .catch(error => {
+            res.send(error)
+        })
+})
 
-    res.status(200).json({
-        success: true,
-        "data": result
-    })
-   
-    
-    
+/**
+ * Petición: Actualizar la información de enciclopedias mediante id
+ * Parámetros: id
+ * Cuerpo: title, description, content, category, date
+ * Respuesta: Información actualizada o mensaje de error
+ */
+router.put('/enciclopedias/:enciclopediaId', async (req, res) => {
+    let id = req.params.enciclopediaId
+    let title = req.body.title
+    let description = req.body.description
+    let content = req.body.content
+    let category = req.body.category
+    let date = req.body.date
+    _controller
+    .updateById(id, title, description, content, category, date)
+        .then((response) => {
+            res.statusCode = 200
+            res.setHeader("Content_type", "application/json")
+            res.status(200).json({
+                success: true,
+                "data": response
+            })
+        })
+        .catch(error => {
+            res.send(error)
+        })
+})
+
+/**
+ * Petición: Eliminar información de enciclopedias mediante id
+ * Parámetros: id
+ * Cuerpo: Vacío
+ * Respuesta: mensaje de éxito o error
+ */
+router.delete('/enciclopedias/:enciclopediaId', async (req, res) => {
+    let id = req.params.enciclopediaId
+    _controller
+    .deleteById(id)
+        .then((response) => {
+            res.statusCode = 200
+            res.setHeader("Content_type", "application/json")
+            res.status(200).json({
+                success: true,
+                "data": response
+            })
+        })
+        .catch(error => {
+            res.send(error)
+        })
 })
 
 module.exports = router;
